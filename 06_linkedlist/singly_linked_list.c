607ec4905376cf20ec6ce695897593e2bee3a381
calmXia <553942917@qq.com>
2019-05-21 22:45:03 +0800

    Create singly_linked_list.c

---
+++ 06_linkedlist/singly_linked_list.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+struct node {
+	int data;
+	struct node * next;
+}
+
+
+//public Node findByValue(int value)
+
+// public Node findByIndex(int index)
+
+
+//无头结点
+//表头部插入
+//这种操作将于输入的顺序相反，逆序
+//public void insertToHead(int value)
+//public void insertToHead(Node newNode)
+
+
+//顺序插入
+//链表尾部插入	
+//public void insertTail(int value)
+
+//public void insertAfter(Node p, int value)
+
+//public void insertAfter(Node p, Node newNode)
+
+//public void insertBefore(Node p, int value)
+
+//public void insertBefore(Node p, Node newNode)
+
+//public void deleteByNode(Node p) 
+
+//public void deleteByValue(int value)	
+
+//public void printAll()
+
+//判断true or false
+//public boolean TFResult(Node left, Node right)
+
+//　判断是否为回文
+//public boolean palindrome()
+
+//带结点的链表翻转
+//public Node inverseLinkList_head(Node p)
+
+//无头结点的链表翻转
+//public Node inverseLinkList(Node p)
+
+//public static Node createNode(int value)
