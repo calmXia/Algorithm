c180cda4b2aecaff7872050424cb6b19c883db9f
calmXia <553942917@qq.com>
2019-05-29 21:39:51 +0800

    Create singly_linked_list.py

---
+++ 06_linkedlist/singly_linked_list.py
@@ -0,0 +1,35 @@
+""" singly linked list
+
+1. Find, insert, delete to singly linked list;
+2.
+
+Author: calm.xia@gmail.com
+"""
+from typing import Optional
+
+class Node(object):
+
+
+    def __init__(self, data:int, next=None):
+        """construction method of class Node
+
+        Parameters:
+            __data: data of Node
+            __next: reference to next Node
+        """
+        self.__data = data
+        self.__next = next
+
+
+class SinglyLinkedList(object):
+
+
+    def __init__(self):
+        """we set a head node for this singly linked list"""
+        self.__head = None
+
+    def find_by_value(self, value:int) -> Optional[Node]:
+        node = self.__head
+        while node != None and node.__data != value:
+            node = node.__next
+        return node
