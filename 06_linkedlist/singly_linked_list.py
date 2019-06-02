6ff03377a3c41fe1afa826bde9eedc3f642169fa
calmXia <553942917@qq.com>
2019-06-02 23:03:16 +0800

    find node by index

--- 06_linkedlist/singly_linked_list.py
+++ 06_linkedlist/singly_linked_list.py
@@ -3,6 +3,9 @@
 1. Find, insert, delete to singly linked list;
 2.
 
+Important:
+    The trail node`s __next is None.
+
 Author: calm.xia@gmail.com
 """
 from typing import Optional
@@ -28,8 +31,19 @@ class SinglyLinkedList(object):
         """__head is the first node of linked list"""
         self.__head = None
 
-    def find_by_value(self, value:int) -> Optional[Node]:
+    def find_by_value(self, value: int) -> Optional[Node]:
         node = self.__head
         while node != None and node.__data != value:
             node = node.__next
         return node
+
+    def find_by_index(self, idx: int) -> Optional[Node]:
+        node = self.__head
+        pos = 0
+        # "node != None" can cover cases "None list" and "last node of list"
+        # pos != idx is the key!
+        while node != None and pos != idx:
+            node = node.__next
+            pos += 1
+        return node
+        
