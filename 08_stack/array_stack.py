9f08f2370e4866fa6bb116ffd716489104a37143
calmXia <553942917@qq.com>
2019-07-01 19:37:32 +0800

    Update array_stack.py

--- 08_stack/array_stack.py
+++ 08_stack/array_stack.py
@@ -6,6 +6,9 @@ Discription:
 
 Author:
     calm.xia@gmail.com
+
+Date:
+    201906
 """
 
 class ArrayStack(object):
@@ -13,13 +16,45 @@ class ArrayStack(object):
     def __init__(self, size: int):
         self.__array_stack = []
         self.__size = size
-        self.__top = 0
+        self.__count = 0
+
+
+    @property
+    def size(self):
+        return self.__size
 
 
     def push(self, value: int):
-        if self.__top >= self.__size -1 :
+        if self.__count >= self.__size :
+            print("stack is already full!")
             return
         else:
             self.__array_stack.append(value)
-            self.__top = self.__top + 1
-        
+            self.__count += 1
+
+
+    def pop(self):
+        if self.__count > 0:
+            self.__count -= 1
+            return self.__array_stack.pop()
+        else:
+            print("stack null!")
+
+    
+    def print_all(self):
+        '''
+        range(start, stop[, step])
+        '''
+        print("stack elements' count = %d, they are:..." % self.__count)
+        for i in range(self.__count, 0, -1):
+            tmp = self.pop()
+            print("stack pop: ", tmp)
+
+
+if __name__ == "__main__":
+    my_array_stack = ArrayStack(5)
+    num = my_array_stack.size
+    print("stack size: %d" % num)
+    for i in range(2,10,2):
+        my_array_stack.push(i)
+    my_array_stack.print_all()
